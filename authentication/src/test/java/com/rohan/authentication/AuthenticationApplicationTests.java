896dd95ad9fb468546a3e49d4866062cf7bfc8e8
Rohan Krishna <rohan@pagefy.org>
2020-10-23 04:02:01 +0530

    Backend First Commit

---
+++ authentication/src/test/java/com/rohan/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
